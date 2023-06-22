77b37e642906f7ff97f5320947d8505fb8ea3809
EngrOgonna <goddeyalaba11@gmail.com>
2023-06-22 01:41:04 -0700

    brain

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and
+returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit.
+If an invalid subreddit is given,
+the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers
+    for a given subreddit.
+    """
+
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
+                                                       subreddit=subreddit)
+
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    res = requests.get(api_uri, headers=user_agent,
+                       allow_redirects=False)
+
+    if res.status_code in [302, 404]:
+        return 0
+
+    return res.json().get('data').get('subscribers')
