a90a4ed7fc9cef6d9b5f7a5732f03ec9dd656a1a
EngrOgonna <goddeyalaba11@gmail.com>
2023-07-15 20:01:18 -0700

    ok

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,45 +1,23 @@
-#!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints
-the top ten hot posts of a subreddit
-"""
 import requests
-import sys
 
+def count_keywords(subreddit, keywords):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-def add_title(hot_list, hot_posts):
-    """ Adds item into a list """
-    if len(hot_posts) == 0:
-        return
-    hot_list.append(hot_posts[0]['data']['title'])
-    hot_posts.pop(0)
-    add_title(hot_list, hot_posts)
+    titles = [post["data"]["title"] for post in data["data"]["children"]]
+    keyword_count = {keyword: 0 for keyword in keywords}
 
+    for title in titles:
+        words = title.lower().split()
+        for keyword in keywords:
+            if keyword.lower() in words:
+                keyword_count[keyword] += 1
 
-def recurse(subreddit, hot_list=[], after=None):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': u_agent
-    }
+    for keyword, count in sorted(keyword_count.items(), key=lambda x: x[1], reverse=True):
+        print(f"{keyword}: {count}")
 
-    params = {
-        'after': after
-    }
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
-
-    if res.status_code != 200:
-        return None
-
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    add_title(hot_list, hot_posts)
-    after = dic['data']['after']
-    if not after:
-        return hot_list
-    return recurse(subreddit, hot_list=hot_list, after=after)
+    after = data["data"]["after"]
+    if after is not None:
+        count_keywords(subreddit, keywords)
